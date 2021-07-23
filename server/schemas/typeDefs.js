bd9c340008d0d59f42311871f3d04a631a286263
Cat Ormerod <catormerod@gmail.com>
2021-07-23 18:22:43 +1000

    type defs added

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,45 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  
+  type User {
+      _id: ID
+      username: String
+      email: String
+      password: String
+      bookCount: Int
+      savedBooks: [Book]!
+  }
+  type Book {
+      authors: String
+      description: String
+      bookId: String
+      image: String
+      title: String
+      link: String
+  }
+  type Auth {
+      token: ID!
+      user: User
+  }
+  type Query {
+      users: [User]
+      user(username: String!): User
+      book(bookId: ID!): Book
+      me: User
+  }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(
+        authors: String
+        description: String
+        bookId: String
+        image: String
+        title: String
+        link: String
+        ): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
